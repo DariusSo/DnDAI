60ea303d62a544529a9a29f82dffbb7079f2b497
Darius <darius.songaila1@gmail.com>
2025-01-14 17:58:47 +0200

    Initial commit

---
+++ src/test/java/com/DnDA/DnDaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DnDA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnDaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
